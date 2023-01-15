10486ef3ec36267a00320be42c8d17ee9bbcbe8c
redbean_jelly <ykw1090@gmail.com>
2023-01-15 22:37:03 +0900

    간단한 Scanner 사용법

---
+++ Lesson01/src/Code02.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Code02 {
+	
+	static void main(String[] args) {
+		
+		int number = 123;
+		
+		Scanner key = new Scanner(System.in);
+		
+		System.out.println("Please enter integer: ");
+		
+		int input = key.nextInt();
+		
+		if(input == number) {
+			System.out.println("Numbers match!!");
+		}else {
+			System.out.println("Numbers do not match!!");
+		}
+		
+	}
+}
